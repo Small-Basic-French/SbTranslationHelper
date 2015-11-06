1cedccb92967ec1b17e0f42053797cc8615342fc
Yan Grenier <ygrenier@ygrenier.com>
2015-11-06 19:51:57 +0100

    Base for the MVVM pattern.

---
+++ sources/SbTranslationHelper/Common/RelayCommandGeneric.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SbTranslationHelper
+{
+    /// <summary>
+    /// Generic command
+    /// </summary>
+    public class RelayCommand<T> : ObservableObject, ICommand
+    {
+        private Action<T> _Execute = null;
+        private Func<T, bool> _CanExecute = null;
+
+        /// <summary>
+        /// Create a new command
+        /// </summary>
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Create a new command
+        /// </summary>
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Raise the CanExecuteChanged event
+        /// </summary>
+        public virtual void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        /// <summary>
+        /// Execute the command
+        /// </summary>
+        public virtual void Execute(object parameter)
+        {
+            _Execute((T)parameter);
+        }
+
+        /// <summary>
+        /// Indicates if the command can be executed
+        /// </summary>
+        public virtual bool CanExecute(object parameter)
+        {
+            return _CanExecute != null ? _CanExecute((T)parameter) : true;
+        }
+
+        /// <summary>
+        /// Event raised when the CanExecute state changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_CanExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (_CanExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+    }
+}
---
+++ sources/SbTranslationHelper/Extensions/PropertyExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbTranslationHelper
+{
+
+    /// <summary>
+    /// Extensions for properties management
+    /// </summary>
+    public static class PropertyExtensions
+    {
+        const string WrongExpressionMessage = "Expression invalid. Must be called with an expression of type n() => PropertyName!";
+        const string WrongUnaryExpressionMessage = "Unary-expression invalid. Must be called with an expression of type n() => PropertyName!";
+
+        /// <summary>
+        /// Helper to find a member expression
+        /// </summary>
+        private static MemberExpression FindMemberExpression<T>(Expression<Func<T>> expression)
+        {
+            if (expression.Body is UnaryExpression)
+            {
+                var unary = (UnaryExpression)expression.Body;
+                var member = unary.Operand as MemberExpression;
+                if (member == null)
+                    throw new ArgumentException(WrongUnaryExpressionMessage, "expression");
+                return member;
+            }
+            return expression.Body as MemberExpression;
+        }
+
+        /// <summary>
+        /// Get the property name from an expression
+        /// </summary>
+        public static String GetPropertyName<T>(this object target, Expression<Func<T>> expression)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+
+            // Find the expression
+            var memberExpression = FindMemberExpression(expression);
+            if (memberExpression == null)
+            {
+                throw new ArgumentException(WrongExpressionMessage, "expression");
+            }
+
+            // Find the member
+            var member = memberExpression.Member as PropertyInfo;
+            if (member == null)
+            {
+                throw new ArgumentException(WrongExpressionMessage, "expression");
+            }
+
+            // Reject the member from another class
+            if (target != null && member.DeclaringType != null)
+            {
+                if (!member.DeclaringType.GetTypeInfo().IsAssignableFrom(target.GetType().GetTypeInfo()))
+                {
+                    throw new ArgumentException(WrongExpressionMessage, "expression");
+                }
+            }
+
+            // Reject static member
+            if (member.GetMethod.IsStatic)
+            {
+                throw new ArgumentException(WrongExpressionMessage, "expression");
+            }
+
+            // Returns the name
+            return member.Name;
+        }
+
+    }
+}
